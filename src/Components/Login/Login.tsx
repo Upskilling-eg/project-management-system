be038b9d26c335b9fffa57ac28a92ac1aea64521
samiya <samii-1989@live.com>
2023-12-11 15:19:39 +0100

    "login Validation"

--- src/Components/Login/Login.tsx
+++ src/Components/Login/Login.tsx
@@ -1,27 +1,32 @@
 
-import { useForm } from 'react-hook-form';
+import { useForm , SubmitHandler} from 'react-hook-form';
 import { Link } from 'react-router-dom';
-
+import logo from '../../assets/images/pms.png'
 
 
 export default function Login() {
+  type FormValues = {
+    email: string,
+    password: string
+  }
 
   const {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm();
-
+  } = useForm<FormValues>();
   
-  const onSubmit = async(data) =>{
+  const onSubmit: SubmitHandler<FormValues> = async(data) =>{
     console.log(data);
   }
 
   return (
-    <div className=' vh-100 auth-container d-flex justify-content-center align-items-center'>
-      <div className='login-wrapper w-50 h-50'>
-      
-      <form onSubmit={handleSubmit(onSubmit)} 
+    <div className=' vh-100 auth-container d-flex justify-content-center align-items-center flex-column'>
+        <div className="text-center mb-2">
+            <img src={logo} alt="" className='img-fluid' />
+        </div>
+        <div className='login-wrapper w-50 h-50'>
+        <form onSubmit={handleSubmit(onSubmit)} 
               action="" className="m-auto w-75 my-5">
                 <p className='text-white'>welcome to PMS</p>
                 <h2 className='title mb-5'>Login</h2>
@@ -73,3 +78,45 @@ export default function Login() {
   )
 }
 
+
+
+
+// import { jwtDecode } from "jwt-decode";
+// import { createContext, useEffect, useState } from "react";
+
+// export interface IAuth {
+//   adminData: string;
+//   saveAdminData: () => void;
+//   requestHeaders: any;
+//   baseUrl: string;
+// }
+// export let AuthContext = createContext<IAuth | null>(null);
+// let requestHeaders = {
+//   Authorization: Bearer ${localStorage.getItem("adminToken")}
+// }
+// let baseUrl = "https://upskilling-egypt.com:3003/api/v1";
+// export default function AuthContextProvider(_props: any) {
+//   //****control admin data state****
+//   const [adminData, setAdminData] = useState<any | null>(null);
+
+//   //*****save admin data ********
+//   let saveAdminData = () => {
+//     let encodedToken = localStorage.getItem("adminToken");
+//     let decodedToken = jwtDecode(encodedToken!);
+//     setAdminData(decodedToken);
+//   };
+
+//   useEffect(() => {
+//     if (localStorage.getItem("adminToken")) {
+//       saveAdminData();
+//     }
+//   }, []);
+  
+//   return (
+//     <AuthContext.Provider
+//       value={{ adminData, saveAdminData, requestHeaders, baseUrl }}
+//     >
+//       {props.children}
+//     </AuthContext.Provider>
+//   );
+// }

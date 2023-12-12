41b5468314b56c3a829e30f26a2316686981eded
samiya <samii-1989@live.com>
2023-12-12 14:00:09 +0100

    ""

--- src/main.tsx
+++ src/main.tsx
@@ -6,12 +6,12 @@ import "bootstrap/dist/js/bootstrap.bundle.js";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import "react-toastify/dist/ReactToastify.css";
 import './index.css'
-import {AuthContextProvider} from './Context/AuthContext';
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <AuthContextProvider>
+
         <App />
-    </AuthContextProvider> 
+
   </React.StrictMode>,
 )

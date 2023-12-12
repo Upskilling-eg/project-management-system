a6d737d396e68a216f1b7bac91eda6e1adfccc5a
samiya <samii-1989@live.com>
2023-12-12 09:41:43 +0100

    "login"

--- src/Components/Login/Login.tsx
+++ src/Components/Login/Login.tsx
@@ -1,10 +1,14 @@
 
 import { useForm , SubmitHandler} from 'react-hook-form';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import logo from '../../assets/images/pms.png'
+import { useContext } from 'react';
+import { AuthContext } from './../../Context/AuthContext';
+import axios from 'axios';
 
-
-export default function Login() {
+const Login: React.FC = ()=> {
+  let { saveAdminData, baseUrl} = useContext(AuthContext);
+  // const navigate = useNavigate();
   type FormValues = {
     email: string,
     password: string
@@ -18,6 +22,17 @@ export default function Login() {
   
   const onSubmit: SubmitHandler<FormValues> = async(data) =>{
     console.log(data);
+    await axios
+    .post(`${baseUrl}/Users/Login`, data)
+    .then((response) => {
+      console.log(response);
+      // const adminToken = localStorage.setItem('adminToken', response.data.token )
+      // saveAdminData();
+      // navigate('/dashboard');
+    })
+    .catch((error)=>{
+      console.log(error);
+    })
   }
 
   return (
@@ -25,64 +40,64 @@ export default function Login() {
         <div className="text-center mb-2">
             <img src={logo} alt="" className='img-fluid' />
         </div>
-        <div className='login-wrapper w-50 h-50'>
-        <form onSubmit={handleSubmit(onSubmit)} 
-              action="" className="m-auto w-75 my-5">
-                <p className='text-white'>welcome to PMS</p>
-                <h2 className='title mb-5'>Login</h2>
-              <div className="form-group my-3">
-                <label className='label-title mb-2'>E-mail</label>
-                    <input 
-                      {...register("email",
-                      { required: true,
-                        pattern:/^[^@ ]+@[^@ ]+\.[^@ .]{2,}$/
-                      })}
-                      type="email"
-                      name="email"
-                      className="form-control custom-input" 
-                      placeholder="Enter your E-mail"/>
+        <div className=' w-50 h-50'>
+          <form onSubmit={handleSubmit(onSubmit)} 
+                action="" className="login-wrapper m-auto w-75 my-5 py-3 px-5">
+                  <p className='text-white'>welcome to PMS</p>
+                  <h2 className='title mb-5'>Login</h2>
+                <div className="form-group my-3">
+                  <label className='label-title mb-2'>E-mail</label>
+                      <input 
+                        {...register("email",
+                        { required: true,
+                          pattern:/^[^@ ]+@[^@ ]+\.[^@ .]{2,}$/
+                        })}
+                        type="email"
+                        name="email"
+                        className="form-control custom-input" 
+                        placeholder="Enter your E-mail"/>
 
-                     {errors.email && errors.email.type === "required" && (<span className='text-danger '>Email is required</span>)}
+                      {errors.email && errors.email.type === "required" && (<span className='text-danger '>Email is required</span>)}
 
-                     {errors.email && errors.email.type === "pattern" && (<span className='text-danger '>Email is invalid</span>)}
-              </div>
-              <div className="form-group my-3">
-                <label className='label-title mb-2'>Password</label>
-                    <input 
-                    {...register("password",
-                     { required: true,
-                      pattern:/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/
-                     })}
-                     type="password"
-                     name="password"                 
-                     className="form-control custom-input"  placeholder="Password"/>
-                    {errors.password && errors.password.type === "required" && (<span className='text-danger'>Password is required</span>)}
-                    {errors.password && errors.password.type === "pattern" && (<span className='text-danger '>password is invalid</span>)}
-                </div>
-                <div className="form-group my-3 d-flex justify-content-end">
-                    {/* <span>Register Now?</span> */}
-                    <Link to='/'
-                    // to='/request-reset-pass'
-                     className='text-white text-decoration-none'>Forgot Password?</Link>
+                      {errors.email && errors.email.type === "pattern" && (<span className='text-danger '>Email is invalid</span>)}
                 </div>
                 <div className="form-group my-3">
-                  <button type="submit" className="btn w-100">
-                    Login
-                  </button>
-                </div>           
-        </form>
-      </div>
-      
+                  <label className='label-title mb-2'>Password</label>
+                      <input 
+                      {...register("password",
+                      { required: true,
+                        pattern:/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/
+                      })}
+                      type="password"
+                      name="password"                 
+                      className="form-control custom-input"  placeholder="Password"/>
+                      {errors.password && errors.password.type === "required" && (<span className='text-danger'>Password is required</span>)}
+                      {errors.password && errors.password.type === "pattern" && (<span className='text-danger '>password is invalid</span>)}
+                  </div>
+                  <div className="form-group my-3 d-flex justify-content-end">
+                      {/* <span>Register Now?</span> */}
+                      <Link to='/'
+                      // to='/request-reset-pass'
+                      className='text-white text-decoration-none'>Forgot Password?</Link>
+                  </div>
+                  <div className="form-group my-3">
+                    <button type="submit" className="btn w-100">
+                      Login
+                    </button>
+                  </div>           
+          </form>
+        </div>      
     </div>
     
   )
 }
-
+export default Login;
 
 
 
 // import { jwtDecode } from "jwt-decode";
 // import { createContext, useEffect, useState } from "react";
+// import { AuthContext } from './Login';
 
 // export interface IAuth {
 //   adminData: string;

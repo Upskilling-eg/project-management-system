0e055973c947ae9ad8f385380814bc6dae0c6830
samiya <samii-1989@live.com>
2023-12-14 14:03:58 +0100

    ""

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,20 @@
 import React from "react";
- import ReactDOM from "react-dom/client";
- import App from "./App.tsx";
- import "bootstrap/dist/css/bootstrap.min.css";
- import "bootstrap/dist/js/bootstrap.bundle.js";
- import "@fortawesome/fontawesome-free/css/all.min.css";
- import "react-toastify/dist/ReactToastify.css";
- import "./index.css";
- import {AuthContextProvider} from "./Context/AuthContext.tsx";
-import ToastContextProvider from './Context/ToastContext';
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.js";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import "react-toastify/dist/ReactToastify.css";
+import "./index.css";
+import { AuthContextProvider } from "./Context/AuthContext.tsx";
+import ToastContextProvider from "./Context/ToastContext";
 
- ReactDOM.createRoot(document.getElementById("root")!).render(
-   <React.StrictMode>
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <AuthContextProvider>
       <ToastContextProvider>
-        <AuthContextProvider>
-         <App />
-        </AuthContextProvider>
+        <App />
       </ToastContextProvider>
-   </React.StrictMode>
- );
+    </AuthContextProvider>
+  </React.StrictMode>
+);

87f6bd1c4933b5360d3ce7dca0e45ad7699126d3
rawan.mohamed7129@gmail.com <rawan.mohamed7129@gmail.com>
2023-12-14 22:17:31 +0200

    update

--- src/Components/Login/Login.tsx
+++ src/Components/Login/Login.tsx
@@ -89,32 +89,20 @@ const Login: React.FC = ()=> {
           <img src={logo} alt="" className='img-fluid' />
         </div>
         <div className=' w-50 h-50'>
-<<<<<<< HEAD
           <form onSubmit={handleSubmit(onSubmit)}
-=======
-          <form onSubmit={handleSubmit(onSubmit)}
->>>>>>> logIn
                 action="" className="login-wrapper m-auto w-75 my-5 py-3 px-5">
                   <p className='text-white'>welcome to PMS</p>
                   <h2 className='title mb-5'>Login</h2>
                 <div className="form-group my-3">
                   <label className='label-title mb-2'>E-mail</label>
-<<<<<<< HEAD
                       <input
-=======
-                      <input
->>>>>>> logIn
                         {...register("email",
                         { required: true,
                           pattern:/^[^@ ]+@[^@ ]+\.[^@ .]{2,}$/
                         })}
                         type="email"
                         name="email"
-<<<<<<< HEAD
                         className="form-control custom-input"
-=======
-                        className="form-control custom-input"
->>>>>>> logIn
                         placeholder="Enter your E-mail"/>
 
               {errors.email && errors.email.type === "required" && (<span className='text-danger '>Email is required</span>)}
@@ -123,62 +111,35 @@ const Login: React.FC = ()=> {
                 </div>
                 <div className="form-group my-3">
                   <label className='label-title mb-2'>Password</label>
-<<<<<<< HEAD
                       <input
-=======
-                      <input
->>>>>>> logIn
                       {...register("password",
                       { required: true,
                         pattern:/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/
                       })}
                       type="password"
-<<<<<<< HEAD
                       name="password"
-=======
-                      name="password"
->>>>>>> logIn
                       className="form-control custom-input"  placeholder="Password"/>
                       {errors.password && errors.password.type === "required" && (<span className='text-danger'>Password is required</span>)}
                       {errors.password && errors.password.type === "pattern" && (<span className='text-danger '>password is invalid</span>)}
                   </div>
-<<<<<<< HEAD
                   <div className="form-group my-3 d-flex justify-content-between">
                       <Link to="/register" className="text-white text-decoration-none">Register Now?</Link>
                       <Link
                       to='/request-reset'
-=======
-                  <div className="form-group my-3 d-flex justify-content-end">
-                      {/* <span>Register Now?</span> */}
-                      <Link to='/'
-                      // to='/request-reset-pass'
->>>>>>> logIn
                       className='text-white text-decoration-none'>Forgot Password?</Link>
                   </div>
                   <div className="form-group my-3">
                     <button type="submit" className="btn w-100">
                       Login
                     </button>
-<<<<<<< HEAD
                   </div>
           </form>
         </div>
     </div>
 
-=======
-                  </div>
-          </form>
-        </div>
-    </div>
-
->>>>>>> logIn
   )
 }
 export default Login;
 
 
 
-<<<<<<< HEAD
-
-=======
->>>>>>> logIn

562c419e3d36120188cb4ff1bc171a5bcc53c8f3
rawan.mohamed7129@gmail.com <rawan.mohamed7129@gmail.com>
2023-12-14 21:13:32 +0200

    update

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,10 @@
-<<<<<<< HEAD
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-=======
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
->>>>>>> logIn
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.js";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import "react-toastify/dist/ReactToastify.css";
-<<<<<<< HEAD
 import "./index.css";
 import { AuthContextProvider } from "./Context/AuthContext.tsx";
 import ToastContextProvider from "./Context/ToastContext";
@@ -25,12 +18,12 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     </AuthContextProvider>
   </React.StrictMode>
 );
-=======
-import './index.css'
+
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 )
->>>>>>> logIn
+
